25aee92e1cb2f25282d370d73a4cfdd4a5c7c6a4
Alexandre Beltzac <beltzac@gmail.com>
2020-09-18 20:46:23 -0300

    Use NewtonsoftJson, facilitates the parsing of json requests

--- Beltzac.Account/Beltzac.Account.Api/Startup.cs
+++ Beltzac.Account/Beltzac.Account.Api/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Beltzac.Account.Data;
+using Beltzac.Account.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -24,7 +26,9 @@ namespace Beltzac.Account.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddScoped<IRepository<Domain.Account>, AccountRepository>();
+            services.AddScoped<IAccountHandler, AccountHandler>();           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
