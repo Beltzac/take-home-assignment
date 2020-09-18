3c570f864eac5de5071aed928624bbacf0d2b475
Alexandre Beltzac <beltzac@gmail.com>
2020-09-18 17:38:42 -0300

    Basic account, without validations

---
+++ Beltzac.Account/Beltzac.Account.Domain/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Beltzac.Account.Domain
+{
+    public class Account
+    {
+        public int Id { get; private set; }
+        public decimal Balance { get; private set; }
+
+        public Account(int id, decimal balance)
+        {
+            Id = id;
+            Balance = balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
