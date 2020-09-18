a5dd2cd3468b4da8db5480c29887dd36c980a9ef
Alexandre Beltzac <beltzac@gmail.com>
2020-09-18 18:15:49 -0300

    Wire BalanceController

--- Beltzac.Account/Beltzac.Account.Api/Controllers/BalanceController.cs
+++ Beltzac.Account/Beltzac.Account.Api/Controllers/BalanceController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Beltzac.Account.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,25 @@ namespace Beltzac.Account.Api.Controllers
     [ApiController]
     public class BalanceController : ControllerBase
     {
+        private readonly IAccountHandler _accountHandler;
+
+        public BalanceController(IAccountHandler accountHandler)
+        {
+            _accountHandler = accountHandler;
+        }
+
+        [HttpGet()]
+        public IActionResult Get([FromQuery(Name = "account_id")] int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var account = _accountHandler.GetAccount(id.Value);
+
+            if (account == null)
+                return NotFound(0);
+
+            return Ok(account.Balance);
+        }
     }
 }
