d5e066ee9fe704980ba2bc1224eb5a899fa81c73
Alexandre Beltzac <beltzac@gmail.com>
2020-09-19 22:52:44 -0300

    The propper way to implement accounts is using event sourcing, so we have a log of transactions

--- Beltzac.Account/Beltzac.Account.Domain/Account.cs
+++ Beltzac.Account/Beltzac.Account.Domain/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.Linq;
 using System.Text;
 
 namespace Beltzac.Account.Domain
@@ -8,22 +9,41 @@ namespace Beltzac.Account.Domain
     public class Account
     {
         public int Id { get; private set; }
-        public decimal Balance { get; private set; }
+        public decimal Balance => CalculateBalance();
+        public List<Transaction> Transactions { get; private set; }
 
         public Account(int id)
         {
             Id = id;
-            Balance = 0;
+            Transactions = new List<Transaction>();
         }
 
         public void Deposit(decimal amount)
         {
-            Balance += amount;
+            var transaction = new Transaction(Transaction.TransactionType.Deposit, amount);
+            Transactions.Add(transaction);
         }
 
         public void Withdraw(decimal amount)
         {
-            Balance -= amount;
+            var transaction = new Transaction(Transaction.TransactionType.Withdraw, -amount);
+            Transactions.Add(transaction);
+        }
+
+        public void Transfer(Account destination, decimal amount)
+        {
+            //From
+            var transactionRemove = new Transaction(Transaction.TransactionType.Transfer, -amount);
+            Transactions.Add(transactionRemove);
+
+            //To
+            var transactionAdd = new Transaction(Transaction.TransactionType.Transfer, amount);
+            destination.Transactions.Add(transactionAdd);
+        }
+
+        private decimal CalculateBalance()
+        {
+            return Transactions.Sum(t => t.Amount);
         }
     }
 }
---
+++ Beltzac.Account/Beltzac.Account.Domain/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beltzac.Account.Domain
+{
+    public class Transaction
+    {
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+
+        public Transaction(TransactionType type, decimal amount)
+        {
+            Type = type;
+            Amount = amount;
+        }
+
+        public enum TransactionType
+        {
+            Deposit,
+            Withdraw,
+            Transfer
+        }
+    }
+}
